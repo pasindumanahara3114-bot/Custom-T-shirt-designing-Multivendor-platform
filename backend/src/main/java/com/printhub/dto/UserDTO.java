08ac433921b9cc2f9da1b93c34f24fa1327cc077
UdulaPahasara <udulapahasara2@gmail.com>
2026-03-27 04:19:41 -0700

    feat: introduce UserDTO and OrderDTO for data transfer operations

---
+++ backend/src/main/java/com/printhub/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.printhub.dto;
+
+import com.printhub.model.User.Role;
+import lombok.*;
+
+/**
+ * UserDTO - Data Transfer Object for User information.
+ * Used for registration and profile responses to avoid exposing sensitive data
+ * like passwords.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+}
